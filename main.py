8697d1d884d9bc3384112ae9565698a81e1c5773
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-10-26 11:07:04 +0100

    Update

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ from pyglet.window import key, mouse
 TICKS_PER_SEC = 60
 
 # Size of sectors used to ease block loading.
-SECTOR_SIZE = 16
+SECTOR_SIZE = 20
 
 WALKING_SPEED = 5
 FLYING_SPEED = 15
 
-GRAVITY = 20.0
-MAX_JUMP_HEIGHT = 1.0 # About the height of a block.
+GRAVITY = 10.0
+MAX_JUMP_HEIGHT = 2 # About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
@@ -159,7 +159,7 @@ class Model(object):
         """ Initialize the world by placing all the blocks.
 
         """
-        n = 80  # 1/2 width and height of world
+        n = 200  # 1/2 width and height of world
         s = 1  # step size
         y = 0  # initial y height
         for x in xrange(-n, n + 1, s):
